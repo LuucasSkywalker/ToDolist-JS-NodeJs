77f80d574532ac188bdd69d672ee8fb480fa9e43
Lucas Cavalcante de Rezende <luucasskywalker@gmail.com>
2024-09-10 22:04:05 -0300

    menu aplicação

--- ToDo.md
+++ ToDo.md
@@ -11,3 +11,14 @@
 Coletar os dados
 Processar os dados ( manipular, alterar ...)
 Apresentar os dados
+
+## Módulos em Node.js
+
+//*Importação de módulos (require, commonJS)
+npm install inquirer
+
+(No Node.js, require é uma função built-in usada para carregar módulos, que podem ser arquivos externos, pacotes instalados globalmente ou módulos internos do próprio Node.js)
+
+//*Biblioteca 'inquirer' para criar prompts interativos
+O inquirer é um pacote popular no Node.js usado para criar interfaces de linha de comando interativas. Ele permite que você faça perguntas ao usuário e receba respostas de maneira amigável e estruturada. Isso é especialmente útil para criar scripts que precisam de entrada do usuário.
+
--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-console.log("Hello word")
+const { select } = require('@inquirer/prompts')   /* 
+require é uma função built-in .
+require é usado para IMPORTANDO UM MODULO/BIBLIOTECA/PACOTES ETC . . 
+Como o código está funcinando :
+quero importar um modulo, e dentro desse modulo tem um objeto dentro da biblioteca 
+que é o inquirer, o @inquirer/prompts 
+dentro da biblioteca inquirer/prompts tem um objeto que quero extrair que é
+o ( select ). . */
+
+const start = async () => {  
+
+    while(true){
+
+            const opcao = await select({
+                message: "Menu >",
+                choices: [
+                    {
+                      name: "Cadastrar meta",
+                      value: "cadastrar"  
+                    },
+                    {
+                      name: "Listar Metas",
+                      value: "listar"
+                    },
+                    {
+                      name: "Sair",
+                      value: "sair"
+                    }
+                ]
+            })
+            
+
+            switch(opcao) {
+                case "cadastrar":
+                    console.log("vamos cadastrar")
+                    break
+                    case "listar":
+                        console.log("vamos listar")
+                        break
+                        case "sair":
+                            return
+            }
+    }
+
+}
+
+start()
